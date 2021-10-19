164f4c9fee59ab991c0b94e153c3024247f61e95
milan vaghasiya <milapvaghasiya25@gmail.com>
2021-10-19 19:09:51 +0100

    first commit

---
+++ src/Biodate.java
@@ -0,0 +1,12 @@
+ class PersonalDetail{
+  public static void main(String[]args){
+  System.out.println("Milan Vaghasiya");
+  System.out.println("29");
+  System.out.println("12 harbourne close watford");
+  System.out.println("percetnce 52");
+  System.out.println("salary 15000");
+    System.out.println("i love git 3");
+}
+
+
+}
